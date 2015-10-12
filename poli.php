f56906db864653df8e6c0be45cda9507f81bd5c8
Dmitry <dmitry1940@mail.ru>
2015-10-12 16:23:43 +0300

    added clone of product block on poli

--- poli.php
+++ poli.php
@@ -12,44 +12,43 @@
             <div class="circles-block">
                 <div class="title-wrap animate-section">
                     <div class="title animate-it">
-                        <p>Что делает выгодным и удобным</p>
-                        <p>сотрудничество с нами?</p>
+                        <p>Виды и характеристики валов</p>
                     </div>
                 </div>
                 <div class="container circles-block-wrap">
                     <div class="row animate-section">
-                        <div class="col-md-3 animate-it">
+                        <div class="col-md-3 animate-delay">
                             <div class="circle-img-wrap">
                                 <img src="images/circle-img3.png" alt="" />
                             </div>
-                            <div class="circle-wrap-description">
-                            <p><span>Постановка</span></p>
-                            <p>новых валов</p>
+                            <div class="circle-wrap-description text-center">
+                                <p><span>Постановка</span></p>
+                                <p>новых валов</p>
                             </div>
                         </div>
-                        <div class="col-md-3 animate-it">
+                        <div class="col-md-3 animate-delay">
                             <div class="circle-img-wrap">
                                 <img src="images/circle-img2.png" alt="" />
                             </div>
-                            <div class="circle-wrap-description">
+                            <div class="circle-wrap-description text-center">
                                 <p><span>Обрезинивание</span></p>
                                 <p>валов заказзчика</p>
                             </div>
                         </div>
-                        <div class="col-md-3 animate-it">
+                        <div class="col-md-3 animate-delay">
                             <div class="circle-img-wrap">
                                 <img src="images/circle-img10.png" alt="" />
                             </div>
-                            <div class="circle-wrap-description">
+                            <div class="circle-wrap-description text-center">
                                 <p><span>Изготовление валов</span></p>
                                 <p>по чертежу</p>
                             </div>
                         </div>
-                        <div class="col-md-3 animate-it">
+                        <div class="col-md-3 animate-delay">
                             <div class="circle-img-wrap">
                                 <img src="images/circle-img11.png" alt="" />
                             </div>
-                            <div class="circle-wrap-description">
+                            <div class="circle-wrap-description text-center">
                                 <p><span>Обмен</span> валов</p>
                             </div>
                         </div>
@@ -59,6 +58,127 @@
         </section>
 
         <section>
+
+            <div class="production-block">
+                <div class="title-wrap animate-section">
+                    <div class="title animate-it">
+                        <p>Виды и характеристики валов</p>
+                    </div>
+                </div>
+                <div class="container production-container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="row animate-section">
+                                <div class="production-item smaller-border cfix animate-it">
+                                    <div class="col-md-4">
+                                        <div class="production-item-img animate-it">
+                                            <img src="images/production-image4.png" alt="" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-7 col-md-offset-1">
+                                        <div class="production-item-content animate-it">
+                                            <div class="production-item-title">Восстановление валов</div>
+                                            <div class="production-item-text">
+                                                <ul>
+                                                    <li>Уровень расхода краски как на новой машине.</li>
+                                                    <li>Выделение минимального количества тепла при печати</li>
+                                                    <li>Оптимальный баланс краска-вода</li>
+                                                    <li>Правильная полоса контакта</li>
+                                                    <li>Стабильные размеры и твердость</li>
+                                                    <li>Увеличенный срок службы</li>
+                                                    <li>Сохранение первоначальной формы, стойкость к термоусадке</li>
+                                                    <li>Минимальная регулировка во время печати</li>
+                                                    <li>Легкая очистка и безаварийная эксплуатация</li>
+                                                    <li>Бархатистый ворс обеспечивает надлежащее сочетание краска-вода</li>
+                                                    <li>Уменьшение засаливания, полошения</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12">
+                            <div class="row animate-section">
+                                <div class="production-item smaller-border cfix animate-it">
+                                    <div class="col-md-4">
+                                        <div class="production-item-img animate-it">
+                                            <img src="images/production-image5.png" alt="" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-7 col-md-offset-1">
+                                        <div class="production-item-content animate-it">
+                                            <div class="production-item-title">Увлажняющие валы</div>
+                                            <div class="production-item-text">
+                                                <ul>
+                                                    <li>Уникальное гидрофильное покрытие, обеспечивающее контроль над увлажняющим раствором</li>
+                                                    <li>Для быстрого запуска и продолжения печати</li>
+                                                    <li>Высокая стойкость к спирту и низкое поверхностное натяжение</li>
+                                                    <li>Стабильность размеров</li>
+                                                    <li>Увеличенный срок службы</li>
+                                                    <li>Стабилизируют производственные условия</li>
+                                                    <li>Уменьшают время для очистки и ухода за валами</li>
+                                                    <li>Поддерживают формирование  эмульсии краска-вода</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12">
+                            <div class="row animate-section">
+                                <div class="production-item smaller-border cfix animate-it">
+                                    <div class="col-md-4">
+                                        <div class="production-item-img animate-it">
+                                            <img src="images/production-image6.png" alt="" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-7 col-md-offset-1">
+                                        <div class="production-item-content animate-it">
+                                            <div class="production-item-title">Траверсные валы (с аксиальным ходом)</div>
+                                            <div class="production-item-text">
+                                                <ul>
+                                                    <li>Уровень расхода краски как на новой машине.</li>
+                                                    <li>Устраняют проблемы шаблонирования</li>
+                                                    <li>Обеспечивают равномерный накат краски на печатную пластину.</li>
+                                                    <li>Улучшают качество оттисков</li>
+                                                    <li>Подходят для машин с высокой скоростью подачи листов.</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12">
+                            <div class="row animate-section">
+                                <div class="production-item smaller-border cfix animate-it">
+                                    <div class="col-md-4">
+                                        <div class="production-item-img animate-it">
+                                            <img src="images/production-image7.png" alt="" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-7 col-md-offset-1">
+                                        <div class="production-item-content animate-it">
+                                            <div class="production-item-title">Красочные валы для УФ печати</div>
+                                            <div class="production-item-text">
+                                                <ul>
+                                                    <li>Анти набухающие свойства для всех типов красок УФ и  очистителей.</li>
+                                                    <li>Отличные свойства переноса краски</li>
+                                                    <li>Стабильность геометрических размеров</li>
+                                                    <li>Легко очищаются и обслуживаются</li>
+                                                    <li>Увеличенный срок службы</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </section>
 
