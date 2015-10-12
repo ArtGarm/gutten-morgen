e2dcb6136fef7ef15d47aa253f6e3d76eafa4e0a
Radiks <radion91@gmail.com>
2015-10-12 10:35:14 +0300

    footer20%

--- footer.php
+++ footer.php
@@ -1,8 +1,20 @@
 	<div class="footer_placeholder"></div>
 	<footer class="footer">
-		<div class="mbox">
-			
-		</div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="logo">
+                        <img src="images/header-logo.png" alt="">
+                    </div>
+                </div>
+                <div class="col-md-4 col-md-offset-1">
+                    <div class="confidental">
+                        <a href="#">Политика конфиденциальности</a href="#">
+                    </div>
+                </div>
+                <div class="col-md-2 col-md-offset-1"></div>
+            </div>
+        </div>
 	</footer>
 </body>
 </html>
