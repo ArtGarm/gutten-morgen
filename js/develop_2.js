7c83589b092e3bc6f990b562b3bd8791ec2437ac
ArtGarm <artem.garmatyk@mail.ru>
2015-10-12 12:45:31 +0300

    fixed slider

--- js/develop_2.js
+++ js/develop_2.js
@@ -2,7 +2,7 @@ function sliderTop(){
     $('.header-slider-wrap-main').slick({
         infinite:true,
         arrows:false,
-        autoplaykey: false,   // true,
+        autoplaykey: true,
         autoplaySpeed:2000,
         fade:true,
         cssEase:'linear',
