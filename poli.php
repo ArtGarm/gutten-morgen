a3e0bb5ba604b6b74fd58b37b5bc159d2aa415da
Dmitry <dmitry1940@mail.ru>
2015-10-12 15:41:02 +0300

    added another circles to poli

--- poli.php
+++ poli.php
@@ -9,7 +9,53 @@
         </section>
 
         <section>
-
+            <div class="circles-block">
+                <div class="title-wrap animate-section">
+                    <div class="title animate-it">
+                        <p>Что делает выгодным и удобным</p>
+                        <p>сотрудничество с нами?</p>
+                    </div>
+                </div>
+                <div class="container circles-block-wrap">
+                    <div class="row animate-section">
+                        <div class="col-md-3 animate-it">
+                            <div class="circle-img-wrap">
+                                <img src="images/circle-img3.png" alt="" />
+                            </div>
+                            <div class="circle-wrap-description">
+                            <p><span>Постановка</span></p>
+                            <p>новых валов</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3 animate-it">
+                            <div class="circle-img-wrap">
+                                <img src="images/circle-img2.png" alt="" />
+                            </div>
+                            <div class="circle-wrap-description">
+                                <p><span>Обрезинивание</span></p>
+                                <p>валов заказзчика</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3 animate-it">
+                            <div class="circle-img-wrap">
+                                <img src="images/circle-img10.png" alt="" />
+                            </div>
+                            <div class="circle-wrap-description">
+                                <p><span>Изготовление валов</span></p>
+                                <p>по чертежу</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3 animate-it">
+                            <div class="circle-img-wrap">
+                                <img src="images/circle-img11.png" alt="" />
+                            </div>
+                            <div class="circle-wrap-description">
+                                <p><span>Обмен</span> валов</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
 
         <section>
@@ -48,7 +94,7 @@
                                                 <p>Разнообразный и богатый опыт сложившаяся структура организации требуют от нас анализа новых предложений. Идейные соображения высшего порядка, а также начало повседневной работы по формированию позиции позволяет выполнять важные задания по разработке систем массового участия. Разнообразный и богатый опыт укрепление и развитие структуры представляет.</p>
                                             </div>
                                             <div class="production-item-button">
-                                                <a href="#" class="button-a">Подробнее</a>
+                                                <a href="#" class="button-a"><span>Подробнее</span></a>
                                             </div>
                                         </div>
                                     </div>
@@ -70,7 +116,7 @@
                                                 <p>Разнообразный и богатый опыт сложившаяся структура организации требуют от нас анализа новых предложений. Идейные соображения высшего порядка, а также начало повседневной работы по формированию позиции позволяет выполнять важные задания по разработке систем массового участия. Разнообразный и богатый опыт укрепление и развитие структуры представляет.</p>
                                             </div>
                                             <div class="production-item-button">
-                                                <a href="#" class="button-a">Подробнее</a>
+                                                <a href="#" class="button-a"><span>Подробнее</span></a>
                                             </div>
                                         </div>
                                     </div>
@@ -90,7 +136,7 @@
                                             <div class="production-item-title">поставка флексогильз</div>
                                             <div class="production-item-text">Разнообразный и богатый опыт сложившаяся структура организации требуют от нас анализа новых предложений. Идейные соображения высшего порядка, а также начало повседневной работы по формированию позиции позволяет выполнять важные задания по разработке систем массового участия. Разнообразный и богатый опыт укрепление и развитие структуры представляет.</div>
                                             <div class="production-item-button">
-                                                <a href="#" class="button-a">Подробнее</a>
+                                                <a href="#" class="button-a"><span>Подробнее</span></a>
                                             </div>
                                         </div>
                                     </div>
