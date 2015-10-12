5b50610df1b81e16af3c79751bc164858c3cc623
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-10-12 10:48:27 +0300

    no message

---
+++ partials/topform.php
@@ -0,0 +1,17 @@
+<div class="top-form-wrap contaner">
+	<div class="top-form">
+		<div class="form-wrap">
+			<form action="" mathod="POST" class="form-top">
+				<div class="input-wrap">
+					<input type="text" name="name" placeholder="Введите Ваше имя">
+				</div>
+				<div class="input-wrap">
+					<input type="text" name="phone" placeholder="Введите Ваш телефон">
+				</div>
+				<div class="input-wrap">
+					<button type="submit"><span>Заказать</span></button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
