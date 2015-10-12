150905d60af9d590238fd6c2ae8b60e94c46b0e6
trolizmaslom <trolizmaslom@gmail.com>
2015-10-12 15:37:34 +0300

    section about_us created

--- partials/about_us.php
+++ partials/about_us.php
@@ -1,9 +1,26 @@
  <div class="about-us">
-    <div class="title-wrap animate-it">
-        <div class="title">
-        <p>Что делает выгодным и удобным</p>
-        <p>сотрудничество с нами?</p>
+    <div class="title-wrap animate-section">
+        <div class="title animate-it">
+        <p>Клиенты о нас</p>
+
         </div>
     </div>
-
+    <div class="container animate-section">
+        <div class="row animate-it">
+            <div class="col-md-3 foto-block-about">
+                <div class="white-around-about">
+                    <div class="container-for-foto">
+                        <img class="foto-for-about-us" src="images/foto-for-about-us.jpg">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-9 text-block-about">
+            <div class="text-bordered-block">
+                <div class="text-about-title"><p><b>Романов Виктор Иванович,</b>бухгалтер, полиграфия “ВИС”</p></div>
+                <div class="text-about"><p>“Равным образом новая модель организационной деятельности способствует подготовки и реализации новых предложений. С другой стороны дальнейшее развитие различных форм деятельности позволяет оценить значение форм развития. ”</p></div>
+            </div>
+            <div class="text-arrow"></div>
+            </div>
+        </div>
+    </div>
 </div>
