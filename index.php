a096fd1849e345a87e6eabe075eaad68069e39b1
Dmitry <dmitry1940@mail.ru>
2015-10-12 13:54:25 +0300

    added basic of production

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 			<?partial('topform');?>
 
 		</section>
+
 		<section>
 			<div class="circles-block">
 				<div class="title-wrap animate-section">
@@ -93,9 +94,86 @@
 				</div>
 			</div>
 		</section>
-		<section>
 
+		<section>
+			<div class="production-block">
+				<div class="title-wrap animate-section">
+					<div class="title animate-it">
+						<p>Что делает выгодным и удобным</p>
+						<p>сотрудничество с нами?</p>
+					</div>
+				</div>
+				<div class="container production-container">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="row">
+								<div class="production-item">
+									<div class="col-md-4">
+										<div class="production-item-img">
+											<img src="images/production-image1.png" alt="" />
+										</div>
+									</div>
+									<div class="col-md-7 col-md-offset-1">
+										<div class="production-item-content">
+											<div class="production-item-title">Восстановление валов</div>
+											<div class="production-item-text">
+												<p>Разнообразный и богатый опыт сложившаяся структура организации требуют от нас анализа новых предложений. Идейные соображения высшего порядка, а также начало повседневной работы по формированию позиции позволяет выполнять важные задания по разработке систем массового участия. Разнообразный и богатый опыт укрепление и развитие структуры представляет.</p>
+											</div>
+											<div class="production-item-button">
+												<a href="#" class="button-a">Подробнее</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-12">
+							<div class="row">
+								<div class="production-item">
+									<div class="col-md-4">
+										<div class="production-item-img">
+											<img src="images/production-image2.png" alt="" />
+										</div>
+									</div>
+									<div class="col-md-7 col-md-offset-1">
+										<div class="production-item-content">
+											<div class="production-item-title">Поставка новых валов</div>
+											<div class="production-item-text">
+												<p>Разнообразный и богатый опыт сложившаяся структура организации требуют от нас анализа новых предложений. Идейные соображения высшего порядка, а также начало повседневной работы по формированию позиции позволяет выполнять важные задания по разработке систем массового участия. Разнообразный и богатый опыт укрепление и развитие структуры представляет.</p>
+											</div>
+											<div class="production-item-button">
+												<a href="#" class="button-a">Подробнее</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-12">
+							<div class="row">
+								<div class="production-item">
+									<div class="col-md-4">
+										<div class="production-item-img">
+											<img src="images/production-image3.png" alt="" />
+										</div>
+									</div>
+									<div class="col-md-7 col-md-offset-1">
+										<div class="production-item-content">
+											<div class="production-item-title">поставка флексогильз</div>
+											<div class="production-item-text">Разнообразный и богатый опыт сложившаяся структура организации требуют от нас анализа новых предложений. Идейные соображения высшего порядка, а также начало повседневной работы по формированию позиции позволяет выполнять важные задания по разработке систем массового участия. Разнообразный и богатый опыт укрепление и развитие структуры представляет.</div>
+											<div class="production-item-button">
+												<a href="#" class="button-a">Подробнее</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</section>
+
 		<section>
 
 		</section>
