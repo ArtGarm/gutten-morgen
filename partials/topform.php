7d81be95954455798869dfeb0ad7216b4845dc12
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-10-12 11:15:49 +0300

    no message

--- partials/topform.php
+++ partials/topform.php
@@ -1,14 +1,14 @@
-<div class="top-form-wrap contaner">
-	<div class="top-form">
-		<div class="form-wrap">
+<div class="top-form-wrap">
+	<div class="top-form container">
+		<div class="form-wrap row">
 			<form action="" mathod="POST" class="form-top">
-				<div class="input-wrap">
+				<div class="input-wrap col-md-4 name">
 					<input type="text" name="name" placeholder="Введите Ваше имя">
 				</div>
-				<div class="input-wrap">
+				<div class="input-wrap col-md-4 phone">
 					<input type="text" name="phone" placeholder="Введите Ваш телефон">
 				</div>
-				<div class="input-wrap">
+				<div class="input-wrap col-md-4">
 					<button type="submit"><span>Заказать</span></button>
 				</div>
 			</form>
